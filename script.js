44671b57a55a1e7971b49d4c9469312caa5327c9
jpcp0614 <jpcp0614@gmail.com>
2023-08-06 15:47:22 -0300

    :sparkles: feat - requirement 4 and 5

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //* ids
 const COLOR_PALETTE_ID = '#color-palette';
+const PIXEL_BOARD_ID = '#pixel-board';
+const SECTION_ID = '#section-id';
 
 //* ---------- Requisito 01 ----------
 const header = document.createElement('header');
@@ -11,19 +13,31 @@ h1.textContent = 'Paleta de Cores';
 headerInBody.appendChild(h1);
 
 //* ---------- Requisito 02 ----------
-// cria a div e adiciona no body
 const divColorPalette = document.createElement('div');
 divColorPalette.id = 'color-palette';
 document.body.appendChild(divColorPalette);
 
 const arrColors = ['black', 'cyan', 'khaki', 'orange'];
 
-// cria div com classe 'color' e as classes de cor
-// através do array de cores
-// e adiciona tal div na color-palette
 for (let i = 0; i < arrColors.length; i += 1) {
   const divColors = document.createElement('div');
   divColors.classList.add('color');
   divColors.classList.add(arrColors[i]);
   document.querySelector(COLOR_PALETTE_ID).appendChild(divColors);
+}
+
+//* ---------- Requisito 04 ----------
+const section = document.createElement('section');
+section.id = 'section-id';
+document.body.appendChild(section);
+
+const divPixelBoard = document.createElement('div');
+divPixelBoard.id = 'pixel-board';
+document.querySelector(SECTION_ID).appendChild(divPixelBoard);
+
+for (let i = 0; i < 25; i += 1) {
+  const divPixel = document.createElement('div');
+  divPixel.classList.add('pixel');
+  document.querySelector(SECTION_ID).style.maxWidth = `${5 * 42}px`; // limitar o tamanho da section (CSS)
+  document.querySelector(PIXEL_BOARD_ID).appendChild(divPixel);
 }
